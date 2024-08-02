fb12cdaa5da76051a25f6574b168a83279dcdde6
rfks <15876610+rfks@users.noreply.github.com>
2024-08-02 14:44:04 +0200

    exposing firebase variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,18 +5,30 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { setLogLevel as setFirestoreLogLevel } from "firebase/firestore";
+setFirestoreLogLevel('debug');
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.APIKEY,
+//   authDomain: process.env.AUTHDOMAIN,
+//   projectId: process.env.PROJECTID,
+//   storageBucket: process.env.STORAGEBUCKET,
+//   messagingSenderId: process.env.MESSAGINGSENDERID,
+//   appId: process.env.APPID,
+//   measurementId: process.env.MEASUREMENTID
+// };
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID
+  apiKey: "AIzaSyCJr-cLr1YYs0WyEp_yJB2mCkjD6wSD9xg",
+  authDomain: "inventory-b97a7.firebaseapp.com",
+  projectId: "inventory-b97a7",
+  storageBucket: "inventory-b97a7.appspot.com",
+  messagingSenderId: "596670716391",
+  appId: "1:596670716391:web:4ed42147aaa4bf9b654542",
+  measurementId: "G-2NR66K67FJ"
 };
-console.log('ENV: ',firebaseConfig)
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
